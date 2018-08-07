65679839e354d0a9669793b948ebf2f90a9b33fa
paulhurrle <paul_hurrle@yahoo.com>
2018-08-06 20:09:26 -0700

    set up formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral'; // converts numbers to a desired format
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
@@ -7,8 +9,8 @@ const ExpenseListItem = ({ description, amount, createdAt, id }) => (
       <h3>{description}</h3>
     </NavLink>
     <ul>
-      <li key={`${description}-${amount}`}>Amount: {amount}</li>
-      <li key={`${description}-${createdAt}`}>Created: {createdAt}</li>
+      <li key={`${description}-${amount}`}>Amount: {numeral(amount / 100).format('$0,0.00')}</li>
+      <li key={`${description}-${createdAt}`}>Created: {moment(createdAt).format('MMMM Do, YYYY')}</li>
     </ul>
   </div>
 );
--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -4,7 +4,7 @@ import ExpenseListItem from '../../components/ExpenseListItem';
 import { expenses } from '../fixtures/expenses';
 
 test('should render ExpenseListItem with expense', () => {
-  const wrapper = shallow(<ExpenseListItem key={expenses[0].id} {...expenses[0]} />);
+  const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
   expect(wrapper).toMatchSnapshot();
 });
 
